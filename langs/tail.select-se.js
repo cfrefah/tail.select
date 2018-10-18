80daee8372e0de7f224d99fef3b07ff07aa50458
cfrefah <fredrik.fahlman@claremont.se>
2018-10-18 08:35:12 +0200

    Create tail.select-se.js
    Swedish translation

---
+++ langs/tail.select-se.js
@@ -0,0 +1,17 @@
+;(function(){
+    if(!window.tail || !window.tail.select){
+        return false;
+    }
+    window.tail.select.strings = {
+        all: "Alla",
+        none: "Inga",
+        actionAll: "Välj alla",
+        actionNone: "Inga",
+        empty: "Inga alternativ",
+        emptySearch: "Inga träffar",
+        limit: "Du kan ej välja fler",
+        placeholder: "Välj...",
+        placeholderMulti: "Välj upp till :limit alternativ...",
+        search: "Sök..."
+    };
+})();
